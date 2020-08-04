9beb35f1a49786b7827785233bfd644280e034d8
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-04 17:54:46 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,10 +5,10 @@ class BirdsController < ApplicationController
   end
 
   def show
-  sighting = Sighting.find_by(id: params[:id])
-  render json: sighting.to_json(:include => {
-    :bird => {:only => [:name, :species]},
-    :location => {:only => [:latitude, :longitude]}
-  }, :except => [:updated_at])
-end
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at])
+  end
 end
